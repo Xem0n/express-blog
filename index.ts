225e0a165014221bb3a0d5807fa38d1265aca404
Xem0n <biedukiewicz87@gmail.com>
2022-05-02 17:32:59 +0200

    add dotenv configuration

--- index.ts
+++ index.ts
@@ -3,26 +3,28 @@
 // setup db (mariadb maybe)
 // add template engine
 
+import 'dotenv/config';
 import express from 'express';
 import session from 'express-session';
 import path from 'path';
+
 import index from './routes/index';
 import admin from './routes/admin';
 
 const app = express();
-const PORT = 8000;
+const port = process.env.PORT ?? 8000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', './views');
 app.set('view engine', 'pug');
 app.locals.basedir = path.join(__dirname, 'views');
 app.use(session({
-    secret: 'dsfsdgdsfSDTFH45'
+    secret: process.env.SESSION_SECRET ?? 'default'
 }));
 
 app.use('/', index);
 app.use('/admin', admin);
 
-app.listen(PORT, () => {
-    console.log(`server is runnig at port: ${PORT}`);
+app.listen(port, () => {
+    console.log(`server is runnig at port: ${port}`);
 });
