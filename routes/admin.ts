95d1abfbc1eb55b8c0853de5130f26f0a0944366
Xem0n <biedukiewicz87@gmail.com>
2022-05-04 02:17:06 +0200

    add basic authentication

--- routes/admin.ts
+++ routes/admin.ts
@@ -1,8 +1,30 @@
-import express from 'express';
+import express, { Request } from 'express';
+import {} from '../types/session';
+
 const router = express.Router();
+
+const authenticate = (req: Request): boolean => {
+    if (req.session.user) {
+        return req.session.user !== null && req.session.user !== undefined;
+    }
+
+    return req.url === '/login';
+};
+
+router.use('/', (req, res, next) => {
+    if (!authenticate(req)) {
+        res.redirect('/admin/login');
+    }
+
+    next();
+});
 
 router.get('/', (req, res) => {
     res.render('admin/index');
+});
+
+router.get('/login', (req, res) => {
+    res.send('login');
 });
 
 export default router;
