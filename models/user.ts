2e26d88284050a5b3cd9dd742133384fea26ff8d
Xem0n <biedukiewicz87@gmail.com>
2022-07-08 16:22:40 -0700

    add fields

--- models/user.ts
+++ models/user.ts
@@ -1,5 +1,18 @@
 class User {
+    id?: number;
 
+    email?: string;
+    password?: string;
+
+    nickname?: string;
+    name?: string;
+    surname?: string;
+
+    register_time?: Date;
+    last_login?: Date;
+
+    verified?: boolean;
+    superadmin?: boolean;
 }
 
 export default User;
