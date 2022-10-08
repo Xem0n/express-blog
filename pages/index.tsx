121edea38e0cf33fbba592e9ef9bc957ccde36ab
Xem0n <biedukiewicz87@gmail.com>
2022-10-08 16:39:14 +0200

    add colons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from 'next';
+
 const Home: NextPage = () => {
-  return <h1>NextJS Blog</h1>
+  return <h1>NextJS Blog</h1>;
 }
 
-export default Home
+export default Home;
