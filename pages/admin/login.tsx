00a6e77dab6db970839f117a100957773a0477c4
Xem0n <biedukiewicz87@gmail.com>
2022-10-18 20:04:08 +0200

    add login form

--- pages/admin/login.tsx
+++ pages/admin/login.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from 'next';
+import Form from '../../components/form';
 
 const Login: NextPage = () => (
-  <>
-  </>
+  <Form
+    title='Sign into your account!'
+    submitText='Log In'>
+      <input 
+        type="email" 
+        name="email"
+        placeholder='Input your email' />
+
+      <input 
+        type="password" 
+        name="password" 
+        placeholder='Input your password'/>
+  </Form>
 );
 
 export default Login;
